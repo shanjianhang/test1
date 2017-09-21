b2466a2f8ee7be0eb80522288cfcc585d42166aa
shanjianhang <18222288979@163.com>
2017-09-21 16:28:56 +0800

    跟新css,html,js文件

--- react-project/js/component.js
+++ react-project/js/component.js
@@ -1,10 +1,42 @@
-ReactDOM.render(
-	React.DOM.h1(
-		{
-			id:"head",
-			dataIndex:"head"
+var HeadCard=React.createClass({
+	propTypes:{
+		title: React.PropTypes.string.isRequired
+	},
+	changeTitle:function(e){
+		this.setState({
+			title: e.target.value
+		})
+	},
+	getDefaultProps:function(){
+		return {
+			title: "这是一个head"
+		}
+	},
+	getInitialState:function(){
+		return {
+			title: this.props.title
+		}
+	},
+	render:function(){
+		var _this=this;
+		return React.createElement("div",{
+			className:"head"
 		},
-		"生活快乐"
-		),
+			React.createElement("div",{
+				className:"head-title-show"
+			},this.state.title),
+			React.createElement("input",{
+				className: "head-title-change",
+				defaultValue: this.props.title,
+				onBlur: this.changeTitle
+			})
+		)
+	}
+});
+ReactDOM.render(
+	React.DOM.div({
+		className: "appMainContain"
+	},
+	<HeadCard />),
 	document.querySelector(".appMain")
 	)
